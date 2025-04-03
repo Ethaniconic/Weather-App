79f8fa96ad5adda4b71f433c10613526ae335e55
Ethan Anthony <e10.anthony345@gmail.com>
2025-04-03 16:10:53 +0530

    The complete Weather app Project. 1) Fetched the API successfully 2) Handeling the errors using try catch

---
+++ weatherapp.js
@@ -0,0 +1,77 @@
+const cDisplay = document.querySelector(".cityDisplay");
+const tDisplay = document.querySelector(".tempDisplay");
+const dDisplay = document.querySelector(".descDisplay");
+const hDisplay = document.querySelector(".humidityDisplay");
+const form = document.querySelector(".weatherForm");
+const card = document.querySelector(".card");
+
+const apiKey = "410bd8e6fcc497d3a45349d820a0518e";
+
+//getting the event. writing it as an aysnc function to use await to get promises.
+form.addEventListener("submit", async event => {
+    event.preventDefault();
+    const input = document.querySelector(".cityInput").value;
+
+    if(input){
+        try{
+            const weatherData = await getWeatherData(input);
+            displayWeather(weatherData);
+        } catch(error){
+            console.error(error);
+            displayError(error);
+        }
+    }else{
+        displayError("Please enter a city");
+    }
+});
+
+//city - it is the placeholder for the variable input
+async function getWeatherData(city){
+    const apiURL = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}`;
+    const response = await fetch(apiURL);
+
+    if(!response.ok){
+        throw new Error(`Could not fetch weather data for city ${city}`);
+
+    }
+
+    return await response.json();
+}
+
+// data - json format structure.
+function displayWeather(data){
+    // console.log(data);
+    // destructuring the data in smaller units.
+    const {name: city, 
+            main: {temp, humidity},
+            weather: [{description}]} = data;
+    console.log(description);
+
+    const html = `
+        <h1 class="cityDisplay">${city}</h1>
+        <p class="tempDisplay">${Math.floor(temp - 273.15)}°C</p>
+        <p class="humidityDisplay">${humidity}</p>
+        <p class="descDisplay">${description}</p>
+    `
+
+    card.textContent = "";
+    card.style.display = "block";
+    card.innerHTML = html;
+    card.style.display = "flex";
+    card.style.flexDirection = "column";
+    // card.style.alignItems = "center";
+    card.style.justifyContent = "center";
+}
+
+function displayError(msg){
+    const eDisplay = document.createElement("p");
+    eDisplay.textContent = msg;
+    eDisplay.classList.add("errorDisplay");
+
+    card.textContent = "";
+    card.style.display = "flex";
+    card.style.flexDirection = "column";
+    card.style.alignItems = "center";
+    card.style.justifyContent = "center";
+    card.appendChild(eDisplay);
+}
